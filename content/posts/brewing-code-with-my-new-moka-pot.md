fdbe3f6b17189a53f178a1835e8497a88ca12e36
TheWizzy <sgcode@gmail.com>
2024-11-14 11:45:20 +0800

    The Moka Pot

---
+++ content/posts/brewing-code-with-my-new-moka-pot.md
@@ -0,0 +1,31 @@
++++
+title = 'Brewing Code with My New Moka Pot'
+date = 2024-11-14T11:36:36+08:00
+draft = false
++++
+
+![Moka Pot](/images/mokapot.jpg)
+
+# Brewing Code with My New Moka Pot ☕💻
+
+You know what is sleek, shiny, and it makes the most dangerous stuff? Coffee, I mean.  
+Yes, I'm talking about my brand-new moka pot! 😍 
+OMG I think this is totally upgrade from my usual instant coffe mix.  
+What have I been missing in this life?!?!
+
+My morning is now something I'm looking forward.  
+Go to the kitchen, spin, pour, mix, brew, happiness engulf.
+
+## My Coding Sessions 🚀
+
+I think I'm more productive now,
+I have better fuel to my engine.
+Generate more code, and happiness.
+
+But I still hate my business partners. 
+
+Anyway...
+Happy coding and happy brewing! ☕💻
+
+
+#Coffee #BetterDay #Productive #ShopShopShop #MokaPot
---
+++ python/compressimage.py
@@ -0,0 +1,28 @@
+# Python utility to compress images in a directory
+# to optimize the image for web page and blog
+
+import os
+import sys
+import argparse
+from PIL import Image
+
+def compress_image(input_dir, quality=85, max_width=800, max_height=800):
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            img_path = os.path.join(input_dir, filename)
+            img = Image.open(img_path)
+            
+            # Resize the image
+            img.thumbnail((max_width, max_height), Image.LANCZOS)
+            
+            # Save the image with the specified quality
+            img.save(img_path, quality=quality, optimize=True)
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Adjust the path to the images folder
+images_dir = os.path.join(current_dir, '../static/images')
+
+# Call the function with the adjusted path
+compress_image(images_dir, quality=85)
